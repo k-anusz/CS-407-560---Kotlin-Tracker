7756f3066c6ab008cd9119b7c8a26a8c8b8f7d4d
k-anusz <70669509+k-anusz@users.noreply.github.com>
2021-04-30 05:26:32 -0400

    Added custom dialog
    Added custom dialog for user money input

--- app/src/main/java/com/example/expensetrackerwithauth/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/expensetrackerwithauth/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.expensetrackerwithauth.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +12,7 @@ import android.widget.Toast
 import com.example.expensetrackerwithauth.R
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.income_input_dialog.view.*
 
 
 class HomeFragment : Fragment() {
@@ -38,20 +40,72 @@ class HomeFragment : Fragment() {
         }
         view.addButton.setOnClickListener { view ->
             Toast.makeText(activity,"Add button Clicked", Toast.LENGTH_SHORT).show()
+            showDialogIncome()
         }
         view.subtractButton.setOnClickListener { view ->
             Toast.makeText(activity,"Subtract button Clicked", Toast.LENGTH_SHORT).show()
+            showDialogSubtract()
         }
 
 
-
-
-
-
-
-
-
         return view
+    }
+
+
+    private fun showDialogIncome(){
+        // inflate the dialog with custom view
+        val mDialogView = LayoutInflater.from(activity).inflate(R.layout.income_input_dialog, null)
+
+        val mBuilder = AlertDialog.Builder(activity)
+            .setView(mDialogView)
+            .setTitle("Add Income")
+        // show dialog
+        val mAlertDialog = mBuilder.show()
+        // save button of custom layout
+        mDialogView.saveButtonIncome.setOnClickListener{
+            // dissmiss dialog
+            mAlertDialog.dismiss()
+            // get text from EditTexts of custom layout
+            val userIncomeInputed = mDialogView.dialogAmountInput.text.toString()
+            val userTypeInputed = mDialogView.dialogTypeInput.text.toString()
+            val userNoteInputed = mDialogView.dialogNoteInput.text.toString()
+
+
+            // do something with the data
+        }
+        // cancel button
+        mDialogView.cancelButtonIncome.setOnClickListener{
+            // dismiss dialog
+            mAlertDialog.dismiss()
+        }
+    }
+
+
+    private fun showDialogSubtract() {
+        // inflate the dialog with custom view
+        val mDialogView = LayoutInflater.from(activity).inflate(R.layout.income_input_dialog, null)
+
+        val mBuilder = AlertDialog.Builder(activity)
+            .setView(mDialogView)
+            .setTitle("Subtract Income")
+        // show dialog
+        val mAlertDialog = mBuilder.show()
+        // save button of custom layout
+        mDialogView.saveButtonIncome.setOnClickListener{
+            // dissmiss dialog
+            mAlertDialog.dismiss()
+            // get text from EditTexts of custom layout
+            val userIncomeForSubtracted = mDialogView.dialogAmountInput.text.toString()
+            val userTypeInputedForSubtracted = mDialogView.dialogTypeInput.text.toString()
+            val userNoteInputedForSubtracted = mDialogView.dialogNoteInput.text.toString()
+
+            // do something with the data
+        }
+        // cancel button
+        mDialogView.cancelButtonIncome.setOnClickListener{
+            // dismiss dialog
+            mAlertDialog.dismiss()
+        }
     }
 
 
