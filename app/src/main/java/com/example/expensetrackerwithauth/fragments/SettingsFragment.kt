64aa3508c82672ff71a19e07a5b941aac6206759
stivi.cangonj <stivi.cangonj@gmail.com>
2021-05-09 02:38:28 -0400

    User can now take a profile photo using the camera after providing persmission

--- app/src/main/java/com/example/expensetrackerwithauth/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/expensetrackerwithauth/fragments/SettingsFragment.kt
@@ -1,17 +1,32 @@
 package com.example.expensetrackerwithauth.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.expensetrackerwithauth.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_settings.*
+import java.util.jar.Manifest
 
 
 class SettingsFragment : Fragment() {
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +35,45 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
+        val camera_button = view.findViewById<Button>(R.id.camera_button)
+
+        camera_button.setOnClickListener() {
+            if(ContextCompat.checkSelfPermission(this.requireContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }
+            else {
+                ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+            }
+        }
         return view
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }
+            else {
+                Toast.makeText(this.requireContext(), "Oops you just denied the permission for camera." + "Don't worry, you can allow it in settings", Toast.LENGTH_LONG)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK) {
+            if(requestCode == CAMERA_REQUEST_CODE) {
+                val thumbNail: Bitmap = data!!.extras!!.get("data") as Bitmap
+                profile_imageView.setImageBitmap(thumbNail)
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,14 +83,15 @@ class SettingsFragment : Fragment() {
         val currentUser = FirebaseAuth.getInstance().currentUser
 
         // Show details
+
         activity?.let {
             person_name.text = currentUser.displayName
             person_email.text = currentUser.email
-            Glide.with(it)
-                .load(currentUser.photoUrl)
-                .placeholder(R.drawable.ic_baseline_person_24)
-                .circleCrop()
-                .into(person_image)
+//            Glide.with(it)
+//                .load(currentUser.photoUrl)
+//                .placeholder(R.drawable.ic_baseline_person_24)
+//                .circleCrop()
+//                .into(profile_imageView.set)
         }
     }
 }
