feff7b4b4bef7cf5754bcbf2fe17b27f3f440930
stivi.cangonj <stivi.cangonj@gmail.com>
2021-05-10 11:44:47 -0400

    Added dummy Pie charts to MoneyIn and MoneyOut Fragments

--- app/src/main/java/com/example/expensetrackerwithauth/fragments/MoneyInFragment.kt
+++ app/src/main/java/com/example/expensetrackerwithauth/fragments/MoneyInFragment.kt
@@ -1,6 +1,7 @@
 package com.example.expensetrackerwithauth.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,16 +9,19 @@ import android.view.ViewGroup
 import com.example.expensetrackerwithauth.R
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.github.aachartmodel.aainfographics.aachartcreator.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_money_in.*
 
 
 class MoneyInFragment : Fragment() {
 
+    val TAG = "MoneyInFragment"
     private val db = Firebase.firestore
     private var userAdapter: UserAdapterPositiveTransactionsOnly? = null
     private val currentUID = FirebaseAuth.getInstance().currentUser.uid
@@ -29,6 +33,36 @@ class MoneyInFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_money_in, container, false)
+
+//        var income_transactions = ArrayList<Int>()
+//        collectionReference
+//            .get()
+//            .addOnSuccessListener { documents ->
+//                for (document in documents) {
+//                    var income = document.toObject<UserTransactions>()
+//                    Log.d(TAG, "onFailure : ${income.userTransactionAmount}")
+//                    income_transactions.add(income.userTransactionAmount!!)
+//                }
+//            }
+        val aaChartView = view.findViewById<AAChartView>(R.id.aa_chart_view)
+
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Pie)
+            .title("Income Vs Outcome")
+            .backgroundColor("#ffffff")
+            .dataLabelsEnabled(false)
+            .legendEnabled(false)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Income")
+                    .data(arrayOf(10000, 5000, 1000 )),
+
+            )
+            )
+
+        //The chart view object calls the instance object of AAChartModel and draws the final graphic
+        aaChartView.aa_drawChartWithChartModel(aaChartModel)
+
 
         return view
     }
--- app/src/main/java/com/example/expensetrackerwithauth/fragments/MoneyOutFragment.kt
+++ app/src/main/java/com/example/expensetrackerwithauth/fragments/MoneyOutFragment.kt
@@ -1,6 +1,7 @@
 package com.example.expensetrackerwithauth.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,17 +9,23 @@ import android.view.ViewGroup
 import com.example.expensetrackerwithauth.R
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_money_in.*
-import kotlinx.android.synthetic.main.fragment_money_in.recycler_view_money_in
 import kotlinx.android.synthetic.main.fragment_money_out.*
 
 
 class MoneyOutFragment : Fragment() {
+
+    private val TAG = "MoneyOutFragment"
 
     private val db = Firebase.firestore
     private var userAdapter: UserAdapterNegativeTransactionsOnly? = null
@@ -31,6 +38,37 @@ class MoneyOutFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_money_out, container, false)
+
+        var outcome_transactions = ArrayList<String>()
+        collectionReference
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    var outcome = document.toObject<UserTransactions>()
+                    Log.d(TAG, "onFailure : ${outcome.userTransactionTitle}")
+                    outcome_transactions.add(outcome.userTransactionTitle!!)
+                }
+            }
+        val aaChartView = view.findViewById<AAChartView>(R.id.aa_chart_view2)
+
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Pie)
+            .title("Types of Expenses")
+            .backgroundColor("#ffffff")
+            .dataLabelsEnabled(false)
+            .legendEnabled(false)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Utilities")
+                    .data(arrayOf(5)),
+                AASeriesElement()
+                    .name("Groceries")
+                    .data(arrayOf(10)),
+                ),
+            )
+
+        //The chart view object calls the instance object of AAChartModel and draws the final graphic
+        aaChartView.aa_drawChartWithChartModel(aaChartModel)
 
         return view
     }
